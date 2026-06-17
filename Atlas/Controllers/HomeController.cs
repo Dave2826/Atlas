7b9df20beb0ada577946962e5bfeb6ba3df3fd98
Dave2826 <david.morales@tecdesoftware.edu.mx>
2026-06-16 19:04:41 -0600

    feat: dashboard v1 and authentication

--- Atlas/Controllers/HomeController.cs
+++ Atlas/Controllers/HomeController.cs
@@ -1,15 +1,36 @@
+using Atlas.Data;
 using Atlas.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace Atlas.Controllers;
 
 [Authorize]
 public class HomeController : Controller
 {
+    private readonly AtlasDbContext _context;
+
+    public HomeController(AtlasDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
+        var userCount = _context.Users.Count();
+        var productCount = _context.Products.Count();
+        var customerCount = _context.Customers.Count();
+        var currentUser = User.Identity?.Name;
+        var currentRole = User.FindFirst(ClaimTypes.Role)?.Value;
+
+        ViewBag.UserCount = userCount;
+        ViewBag.ProductCount = productCount;
+        ViewBag.CustomerCount = customerCount;
+        ViewBag.CurrentUser = currentUser;
+        ViewBag.CurrentRole = currentRole;
+
         return View();
     }
 
